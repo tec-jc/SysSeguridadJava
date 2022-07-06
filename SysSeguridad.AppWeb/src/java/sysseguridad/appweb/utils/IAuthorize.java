a6d96fa6d9b57da8052dabdcf1906472a2851077
Julio César Tula <jc_tula@outlook.com>
2022-07-06 15:38:18 -0600

    Programación de IAuthorize, SessionUser y Utilidad

--- SysSeguridad.AppWeb/src/java/sysseguridad/appweb/utils/IAuthorize.java
+++ SysSeguridad.AppWeb/src/java/sysseguridad/appweb/utils/IAuthorize.java
@@ -1,5 +1,8 @@
 package sysseguridad.appweb.utils;
 
+import java.io.IOException;
+import javax.servlet.ServletException;
+
 public interface IAuthorize {
-    
+    void authorize() throws ServletException, IOException;
 }
